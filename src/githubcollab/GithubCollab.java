c2cf4cf2c87174d4658484fc3beb6ee079859b5e
mitchell.pople@gmail.com <>
2018-10-24 15:49:17 -0400

    Hello!

---
+++ src/githubcollab/GithubCollab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubcollab;
+
+/**
+ *
+ * @author MP26
+ */
+public class GithubCollab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello!  This is Mitchell");
+    }
+    
+}
